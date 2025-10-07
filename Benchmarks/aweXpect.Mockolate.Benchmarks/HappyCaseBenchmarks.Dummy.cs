658511628d5166253539164851fa03d26e0f7a11
Valentin Breuß <vbreuss@gmail.com>
2025-10-07 19:10:37 +0200

    initial: Set up repository
    Use the T6e template from aweXpect

---
+++ Benchmarks/aweXpect.Mockolate.Benchmarks/HappyCaseBenchmarks.Dummy.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace aweXpect.Mockolate.Benchmarks;
+
+/// <summary>
+///     This is a dummy benchmark in the Mockolate template.
+/// </summary>
+public partial class HappyCaseBenchmarks
+{
+	[Benchmark]
+	public TimeSpan Dummy_aweXpect()
+		=> TimeSpan.FromSeconds(10);
+}
---
+++ Docs/pages/00-index.md
@@ -0,0 +1,5 @@
+﻿# [aweXpect.Mockolate](https://github.com/aweXpect/aweXpect.Mockolate) [![Nuget](https://img.shields.io/nuget/v/aweXpect.Mockolate)](https://www.nuget.org/packages/aweXpect.Mockolate)
+
+Template for extension projects for [aweXpect](https://github.com/aweXpect/aweXpect).
+
+{README}
