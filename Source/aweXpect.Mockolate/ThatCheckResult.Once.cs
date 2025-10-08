b8521cdfc8aec3bfc55c5e61641d8431396da03d
Valentin Breuß <vbreuss@gmail.com>
2025-10-08 09:59:56 +0200

    feat: add first initial example expectation `Once`

---
+++ Source/aweXpect.Mockolate/ThatCheckResult.Once.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Text;
+using aweXpect.Core;
+using aweXpect.Core.Constraints;
+using aweXpect.Helpers;
+using aweXpect.Results;
+using Mockolate.Checks;
+
+namespace aweXpect;
+
+/// <summary>
+///     See https://awexpect.com/docs/extensions/write-extensions
+/// </summary>
+public static class ThatCheckResult
+{
+	/// <summary>
+	///     Verifies that the <paramref name="subject" /> is an absolute path.
+	/// </summary>
+	public static AndOrResult<CheckResult<TMock>, IThat<CheckResult<TMock>>> Once<TMock>(
+		this IThat<CheckResult<TMock>> subject)
+		=> new(subject.Get().ExpectationBuilder.AddConstraint((expectationBuilder, it, grammars)
+				=> new CheckResultConstraint<TMock>(expectationBuilder, it, grammars)),
+			subject);
+
+	private sealed class CheckResultConstraint<TMock>(
+		ExpectationBuilder expectationBuilder,
+		string it,
+		ExpectationGrammars grammars)
+		: ConstraintResult.WithValue<CheckResult<TMock>>(grammars),
+			IValueConstraint<CheckResult<TMock>>
+	{
+		private int _count = 1;
+		
+		public ConstraintResult IsMetBy(CheckResult<TMock> actual)
+		{
+			Actual = actual;
+			Outcome = actual.Verify(interactions =>
+			{
+				var context = string.Join(", ", interactions.Select(x => x.ToString()));
+				expectationBuilder.UpdateContexts(contexts => contexts.Add(
+					new ResultContext("Interactions", () => context)));
+				_count = interactions.Length;
+				return interactions.Length == 1;
+			})
+				? Outcome.Success
+				: Outcome.Failure;
+			return this;
+		}
+
+		protected override void AppendNormalExpectation(StringBuilder stringBuilder, string? indentation = null)
+			=> stringBuilder.Append(Actual?.Expectation).Append(" once");
+
+		protected override void AppendNormalResult(StringBuilder stringBuilder, string? indentation = null)
+			=> stringBuilder.Append("found ").Append(it).Append(" ").Append(_count).Append(" times");
+
+		protected override void AppendNegatedExpectation(StringBuilder stringBuilder, string? indentation = null)
+			=> stringBuilder.Append(Actual?.Expectation).Append(" not once");
+
+		protected override void AppendNegatedResult(StringBuilder stringBuilder, string? indentation = null)
+			=> stringBuilder.Append(it).Append(" was");
+	}
+}
---
+++ Tests/aweXpect.Mockolate.Tests/ThatCheckResultIs.OnceTests.cs
@@ -0,0 +1,71 @@
+﻿using Mockolate;
+using Xunit.Sdk;
+
+namespace aweXpect.Mockolate.Tests;
+
+public sealed partial class ThatCheckResultIs
+{
+	public sealed class OnceTests
+	{
+		[Fact]
+		public async Task WhenInvokedOnce_ShouldSucceed()
+		{
+			var mock = Mock.Create<IMyService>();
+
+			mock.Object.MyMethod(1, false);
+
+			async Task Act()
+				=> await That(mock.Invoked.MyMethod(1, false)).Once();
+
+			await That(Act).DoesNotThrow();
+		}
+
+		[Fact]
+		public async Task WhenInvokedMoreThanOnce_ShouldFail()
+		{
+			var mock = Mock.Create<IMyService>();
+
+			mock.Object.MyMethod(1, false);
+			mock.Object.MyMethod(1, false);
+
+			async Task Act()
+				=> await That(mock.Invoked.MyMethod(1, false)).Once();
+
+			await That(Act).Throws<XunitException>()
+				.WithMessage("""
+					Expected that mock.Invoked.MyMethod(1, false)
+					invoked method MyMethod(1, False) once,
+					but found it 2 times
+
+					Interactions:
+					[0] invoke method aweXpect.Mockolate.Tests.ThatCheckResultIs.IMyService.MyMethod(1, False), [1] invoke method aweXpect.Mockolate.Tests.ThatCheckResultIs.IMyService.MyMethod(1, False)
+					""");
+		}
+
+		[Fact]
+		public async Task WhenInvokedNever_ShouldFail()
+		{
+			var mock = Mock.Create<IMyService>();
+
+
+			async Task Act()
+				=> await That(mock.Invoked.MyMethod(1, false)).Once();
+
+			await That(Act).Throws<XunitException>()
+				.WithMessage("""
+					Expected that mock.Invoked.MyMethod(1, false)
+					invoked method MyMethod(1, False) once,
+					but found it 0 times
+
+					Interactions:
+					
+					""");
+		}
+	}
+
+	public interface IMyService
+	{
+		int MyProperty { get; set; }
+		int MyMethod(int value, bool flag);
+	}
+}
