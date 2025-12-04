40f2948a8465bf2e05132f17166aea8e611dd0cb
Valentin Breuß <vbreuss@gmail.com>
2025-12-04 08:55:41 +0100

    feat: add `AllSetupsAreUsed` (#40)
    This PR adds a new `AllSetupsAreUsed` verification method to the aweXpect.Mockolate library. This feature allows users to verify that all mocked method setups have been invoked during testing.
    
    ### Key changes
    - Adds `AllSetupsAreUsed` extension method for mock verification
    - Updates the Mockolate package dependency from 0.38.1 to 0.40.0
    - Includes comprehensive test coverage for success, single failure, multiple failures, and negated scenarios

---
+++ Tests/aweXpect.Mockolate.Tests/ThatMockVerifyIs.AllSetupsAreUsedTests.cs
@@ -0,0 +1,98 @@
+﻿using Mockolate;
+using Mockolate.Verify;
+using Xunit.Sdk;
+
+namespace aweXpect.Mockolate.Tests;
+
+public sealed partial class ThatMockVerifyIs
+{
+	public sealed class AllSetupsAreUsedTests
+	{
+		[Fact]
+		public async Task WhenAllInvocationsWereVerified_ShouldNotThrow()
+		{
+			IMyService mock = Mock.Create<IMyService>();
+			mock.SetupMock.Method.DoWork(Match.Any<int>());
+
+			mock.DoWork(1);
+			mock.DoWork(2);
+
+			async Task Act()
+			{
+				await That(mock.VerifyMock).AllSetupsAreUsed();
+			}
+
+			await That(Act).DoesNotThrow();
+		}
+
+		[Fact]
+		public async Task WhenOneSetupIsNotUsed_ShouldThrow()
+		{
+			IMyService mock = Mock.Create<IMyService>();
+			mock.SetupMock.Method.DoWork(Match.With(1));
+			mock.SetupMock.Method.DoWork(Match.With(2));
+
+			mock.DoWork(1);
+
+			async Task Act()
+			{
+				await That(mock.VerifyMock).AllSetupsAreUsed();
+			}
+
+			await That(Act).Throws<XunitException>()
+				.WithMessage("""
+				             Expected that the ThatMockVerifyIs.IMyService mock
+				             has used all setups,
+				             but the following setup was not used:
+				              - void aweXpect.Mockolate.Tests.ThatMockVerifyIs.IMyService.DoWork(2 value)
+				             """);
+		}
+
+		[Fact]
+		public async Task WhenMultipleSetupsAreNotUsed_ShouldThrow()
+		{
+			IMyService mock = Mock.Create<IMyService>();
+			mock.SetupMock.Method.DoWork(Match.With(1));
+			mock.SetupMock.Method.DoWork(Match.With(2));
+			mock.SetupMock.Method.DoWork(Match.With(3));
+
+			mock.DoWork(2);
+
+			async Task Act()
+			{
+				await That(mock.VerifyMock).AllSetupsAreUsed();
+			}
+
+			await That(Act).Throws<XunitException>()
+				.WithMessage("""
+				             Expected that the ThatMockVerifyIs.IMyService mock
+				             has used all setups,
+				             but the following 2 setups were not used:
+				              - void aweXpect.Mockolate.Tests.ThatMockVerifyIs.IMyService.DoWork(3 value)
+				              - void aweXpect.Mockolate.Tests.ThatMockVerifyIs.IMyService.DoWork(1 value)
+				             """);
+		}
+
+		[Fact]
+		public async Task Negated_WhenAllSetupsAreUsed_ShouldThrow()
+		{
+			IMyService mock = Mock.Create<IMyService>();
+			mock.SetupMock.Method.DoWork(Match.Any<int>());
+
+			mock.DoWork(1);
+			mock.DoWork(2);
+
+			async Task Act()
+			{
+				await That(mock.VerifyMock).DoesNotComplyWith(it => it.AllSetupsAreUsed());
+			}
+
+			await That(Act).Throws<XunitException>()
+				.WithMessage("""
+				             Expected that the ThatMockVerifyIs.IMyService mock
+				             has not used all setups,
+				             but all were
+				             """);
+		}
+	}
+}
